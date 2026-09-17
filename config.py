36d20b5a4bd03de1419b09d4cbac9c135e4c646b
akshayakondapalle-png <akshayakondapalle@gmail.com>
2026-09-17 15:02:21 +0530

    Prioritize CUDA device detection for Kaggle/Colab GPU environments

--- config.py
+++ config.py
@@ -31,10 +31,10 @@ MODEL_SAVE_PATH = 'best_bert_sentiment_model.pt'
 
 # Device selection (Apple Silicon MPS -> CUDA -> CPU)
 def get_device():
-    if torch.backends.mps.is_available():
-        return torch.device('mps')
-    elif torch.cuda.is_available():
+    if torch.cuda.is_available():
         return torch.device('cuda')
+    elif torch.backends.mps.is_available():
+        return torch.device('mps')
     else:
         return torch.device('cpu')
 
